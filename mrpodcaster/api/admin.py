8e92ec0cd55087974cff58269bda837b325099df
Daniel Kazakov <danielkgb@email.com>
2024-05-18 12:58:06 +0300

    state changes

--- mrpodcaster/api/admin.py
+++ mrpodcaster/api/admin.py
@@ -1,1 +1,13 @@
+from django.contrib import admin
+from .models import Podcast, TelegramConversationView
+
+
 # Register your models here.
+@admin.register(Podcast)
+class PodcastAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TelegramConversationView)
+class TelegramConversationViewAdmin(admin.ModelAdmin):
+    pass
